2f3b0b24ddfc61f2bd4f72471da97c9997fc3999
Mayur K Setty <settymayurk@gmail.com>
2026-01-16 10:10:56 +0530

    added globe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,10 @@
+import GlobeHero from '@/components/hero/GlobeHero';
+
 export default async function Home() {
-  const BACKENDURL = process.env.NEXT_PUBLIC_BACKENDURL;
-
-  if (!BACKENDURL) {
-    throw new Error('Required environment variable NEXT_PUBLIC_BACKENDURL is not set.');
-  }
   
-  const res = await fetch(`${BACKENDURL}/reports`, {
-    cache: 'no-store',
-  });
-  const data = await res.json();
-
   return (
     <main>
-      <h1>Flood Reports Testing</h1>
-      <p>Displaying the latest flood reports fetched from the server.</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+        <GlobeHero />
     </main>
   );
 }
